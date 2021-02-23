28706e911ab89319d48ffb76275de09bbdbec609
nick <nicholas_scalione@yahoo.com>
2021-02-23 10:31:37 -0500

    commit tree d08d74f9b06491a185b0437e4fbf2ae15560a8f4

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 class App < Sinatra::Base
+	
+		get '/hello' do
+			erb :hello
+		  end
+	  
+		  get "/goodbye" do
+			erb :goodbye
+		  end 
 
-	get '/' do
-		erb :index
+		  get '/date' do
+			erb :date
 	end
-
-
 end
